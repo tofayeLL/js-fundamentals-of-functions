e961ab8b1b17c745816c23e55bdcf76e349e1810
tofayeLL <ahmedtofayel132@gmail.com>
2024-02-03 00:41:42 +0600

    learn and practice about return in js function how iot works

---
+++ return.js
@@ -0,0 +1,31 @@
+
+function cutHalf(num){
+    let half = num / 2;
+    return half;
+}
+
+let output2 = cutHalf(50);
+console.log(output2);
+
+
+function tenTimes(num){
+    let result = num * 20;
+    return result;
+}
+// tenTimes(20);
+let output1 = tenTimes(20);
+console.log(output1);
+// another number pass
+let bigNum = tenTimes(99);
+console.log(bigNum);
+// new variable declare and pass value by use variable as a parameter
+let smallNum = 4;
+let small = tenTimes(smallNum);
+console.log(small);
+
+// so return kaj kore like je function call kore value pass kore sekhane return tar valu ta rakhe ...but jodi amra value ta diye kono kaj korte chay so setake amra abr ekta new variable declare kore sekhane rakhte pari.
+
+
+
+
+
