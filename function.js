5589bf1a3d6aeaad27da8493e76cdf61d2dc6f87
tofayeLL <ahmedtofayel132@gmail.com>
2024-02-02 22:51:49 +0600

    learn and practice about function declare and call

---
+++ function.js
@@ -0,0 +1,22 @@
+// how to call a function in js
+// function declare
+function fanOffKor(){
+    console.log('bosha theke uthe dara');
+    console.log('walk towards fan');
+    console.log('click on the switch to turn off the fan');
+}
+// call the function
+fanOffKor();
+
+/* ---function basic structure-- */
+/* 
+----function name(){}-----
+*/
+//declare a function
+function brushTeeth(){
+    console.log('pick up the brush');
+    console.log('add toothpaste');
+    console.log('ghosho and ghosho and fena felo');
+}
+// call the function
+brushTeeth();
