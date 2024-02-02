5e22e71cdf838c6721d303851a612a6e33dea313
tofayeLL <ahmedtofayel132@gmail.com>
2024-02-02 23:39:18 +0600

    learn and practice about parameter in js

---
+++ parameter.js
@@ -0,0 +1,55 @@
+
+/* function vaatKhaow(){
+    console.log('before eat wash your hands');
+    console.log('bosho');
+    console.log('plate  akhabar dabar naw');
+    console.log('gopat gopat kore khaw');
+
+}
+vaatKhaow(); */
+
+// square number
+/* 
+
+3*3=9;
+2*2=4;
+4*4=16;
+
+ */
+
+function square(number) {
+    console.log('Number is:', number)
+    let borgo = number * number;
+    console.log('Square of the number is:', borgo);
+
+}
+
+square(4);
+square(5);
+
+console.log('-----------');
+square(6);
+square(7);
+square(50);
+
+
+
+function add(num1, num2) {
+    console.log(num1, num2);
+    sum = num1 + num2;
+    console.log('sum of the number is:', sum)
+
+}
+
+
+add(4, 5);
+add(7, 8);
+
+
+function addAll(a, b, c, d, e) {
+    let total = a + b + c + d + e;
+    console.log('total sum:', total);
+}
+addAll(2, 3, 4, 8, 10);
+addAll(5, 73, 98, 24, 6);
+
