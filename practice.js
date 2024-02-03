00773f0e523f8c0a043269e3802f45a02f5c1a71
tofayeLL <ahmedtofayel132@gmail.com>
2024-02-03 15:08:54 +0600

    finished practice task one

---
+++ practice.js
@@ -0,0 +1,11 @@
+/* ---practice one---- */
+/* question: Take four parameters. Multiply the four numbers and then return the result */
+
+function multiply(num1, num2, num3, num4){
+    let multiply = num1 * num2 * num3 * num4;
+    return multiply;
+
+}
+
+let result = multiply(4, 3, 2, 5);
+console.log(result);
