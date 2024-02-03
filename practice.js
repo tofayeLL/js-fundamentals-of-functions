906ffce4561e9896ff60f48e12268bd157c79cc5
tofayeLL <ahmedtofayel132@gmail.com>
2024-02-03 15:21:00 +0600

    finished practice task two

--- practice.js
+++ practice.js
@@ -1,7 +1,7 @@
 /* ---practice one---- */
 /* question: Take four parameters. Multiply the four numbers and then return the result */
 
-function multiply(num1, num2, num3, num4){
+/* function multiply(num1, num2, num3, num4){
     let multiply = num1 * num2 * num3 * num4;
     return multiply;
 
@@ -9,3 +9,26 @@ function multiply(num1, num2, num3, num4){
 
 let result = multiply(4, 3, 2, 5);
 console.log(result);
+ */
+
+
+/* ---practice task two---- */
+/* question: Take a number if the number is odd multiply it by 2 and return the result. If the number is even divide it by two and return the result. */
+
+function evenOdd(num){
+    console.log('number is:', num);
+    if(num % 2 === 0){
+        console.log('number is even number:', num);
+        return num * 2;
+    }
+    else{
+        console.log('number is odd number:', num);
+        return num / 2;
+    }
+
+}
+
+let result = evenOdd(5);
+let result1 = evenOdd(4);
+let result2 = evenOdd(7);
+console.log('The result is:', result, result1, result2);
